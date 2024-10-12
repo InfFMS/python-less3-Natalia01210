688705f9b0b7fc5dfd2d8782e53b170f2ec13109
Мальцева Наталья Витальевна <n.maltseva2@main.nsu.ru>
2024-10-12 12:52:37 +0700

    Дорешала задачи

--- task10.py
+++ task10.py
@@ -2,6 +2,33 @@
 # Определить минимальное и максимальное среди простых чисел
 # (которые делятся на сами не себя и на 1).
 # Если таких чисел не было, вывести "нет".
+def F(n):
+    if(n == 1):
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if(n % i == 0):
+            return False
+    return True
 
+N = int(input())
+if(N == 0):
+    print("нет")
+    exit()
+now = int(input())
+mx, mn = now, now
+for i in range(N - 1):
+    now = int(input())
+    if(F(now)):
+        if(F(mn)):
+            mn = min(mn, now)
+            mx = max(mx, now)
+        else:
+            mn = now
+            mx = now
+if(not F(mn)):
+    print("нет")
+    exit()
+print("Минимальное простое число:", mn)
+print("Максимальное простое число:", mx)
 
 
--- task9.py
+++ task9.py
@@ -9,17 +9,18 @@ now = int(input())
 mn, mx = now, now
 for i in range(N - 1):
     now = int(input())
-    if(now % 3 == 0):
-        if(mn % 3 == 0):
+    if(now % 3 == 0 and now > 9 and now < 100):
+        if(mn % 3 == 0 and mn > 9 and mn < 100):
             mn = min(mn, now)
         else:
             mn = now
-        if(mx % 3 == 0):
+        if(mx % 3 == 0 and mx > 9 and mx < 100):
             mx = max(mx, now)
         else:
             mx = now
-if(mn % 3 != 0):
+
+if(mn % 3 != 0 or mn < 10 or mn > 99):
     print("нет")
     exit()
-print("Минимальное число:", mn)
-print("Максимальное число", mx)
+print("Минимальное двузначное число, делящееся на 3:", mn)
+print("Максимальное двузначное число, делящееся на 3:", mx)
